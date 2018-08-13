cd8641a27de3481de2a6dfc8132706c39ef527b0
666dzy666 <568314913@qq.com>
2018-08-13 23:35:41 +0800

    log message

--- 下位机/主板/HARDWARE/PWM/pwm.h
+++
@@ -1,5 +0,0 @@
-#ifndef __PWM_H
-#define __PWM_H
-void move(int l_speed,int r_speed);
-void fire(int speed);
-#endif
--- 下位机/副板/HARDWARE/gprs/gprs.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIMER_H
-#define __TIMER_H
-#include "sys.h"	  
-void GSM_Init(void);
-void GSM_Send(u8 *p1,u8 *p2);
-char hand(char *ptr);
-void clear_rec_data(void);
-#endif
